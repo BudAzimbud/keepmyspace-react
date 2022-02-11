cc1fa1059af3e5949e6dcb07d840b6e69fe91fc2
Budazimbud <budazimbud@gmail.com>
2022-02-11 11:02:03 +0700

    change component to stateless and register feature has been used

--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import RegisterFormComponent from '../components/RegisterFormComponent';
 import { Container, Col } from 'react-bootstrap'
-import registerImage from '../images/registerImage.svg'
 import { connect } from "react-redux";
 import { createUser } from '../actions/ActionUser';
 import './RegisterPage.css'
-
 import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom';
+
+
 
 const mapStateToProps = (state) => {
     return {
@@ -14,42 +15,61 @@ const mapStateToProps = (state) => {
         createUserError: state.users.createUserError
     }
 }
-class RegisterPages extends Component {
+function RegisterPages(props) {
+    const history = useNavigate()
+    const [isLoading, setIsLoading] = useState(false)
 
-
-    handleSubmit(data) {
-        this.props.dispatch(createUser(data))
+    const handleSubmit = (data) => {
+        props.dispatch(createUser(data));
+        setIsLoading(true)
     }
 
-    render() {
-        if (this.props.createUserResponse.data) {
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'Your work has been saved',
-                showConfirmButton: false,
-                timer: 1500
-            })
-        }
 
 
-        return (
-            <div className="register-container">
+    if (props.createUserResponse.data) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        history('/login', { state: true })
 
-                <Container >
-                    <h2>Join With Us</h2>
-                    <div className='d-flex justify-content-center'>
-                        <img src={registerImage} width='100' className="mt-4" alt="image" />
-                    </div>
-                    <div className='d-flex justify-content-center'>
-                        <Col xs lg={5} >
-                            <RegisterFormComponent onSubmit={(data) => { this.handleSubmit(data) }} />
-                        </Col>
-                    </div>
-                </Container>
-            </div>
-        );
     }
+
+    if (props.createUserError.statusCode === 409) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'coba pake email lain',
+            footer: '<a href="">Why do I have this issue?</a>'
+        })
+        setTimeout(() => {
+            window.location.reload();
+        }, 2000)
+
+    }
+
+
+    return (
+        <div className="register-container">
+
+            <Container >
+                <h2>Join With Us</h2>
+                <div className='d-flex justify-content-end'>
+                    <h2>Keep your family</h2>
+                    <hr></hr>
+                    <p>with KEEP MY SPACE</p>
+                </div>
+                <div className='d-flex justify-content-end'>
+                    <Col xs lg={5} >
+                        <RegisterFormComponent onSubmit={(data) => { handleSubmit(data) }} isLoading={isLoading} />
+                    </Col>
+                </div>
+            </Container>
+        </div>
+    );
 }
 
 export default connect(mapStateToProps, null)(RegisterPages);
