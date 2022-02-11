b9fd4a0bcb9b6a9ab4cb6c8eaf8babe2c2c89e9c
Budazimbud <budazimbud@gmail.com>
2022-02-11 14:12:39 +0700

    auto redirect while have been login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import NavbarComponent from './components/Navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPages from './containers/LoginPages'
 import RegisterPages from './containers/RegisterPage';
 import HomePage from './containers/Home';
+
+
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter forceRefresh={true}>
         <NavbarComponent />
         <Routes>
+          {localStorage.getItem('token') ?
+            <Route
+              path="/login"
+              element={<Navigate to="/dashboard" />}
+            /> : <Route exact path='/' element={< HomePage />}></Route>
+          }
           <Route exact path='/login' element={< LoginPages />}></Route>
           <Route exact path='/register' element={< RegisterPages />}></Route>
           <Route exact path='/' element={< HomePage />}></Route>
-
-
         </Routes>
       </BrowserRouter>
     </div>
